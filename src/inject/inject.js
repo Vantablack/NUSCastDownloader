66fb6bd30670f4273cbc7c27dbd90cf22839ede4
Vantablack <chongyaowei@gmail.com>
2020-01-15 14:54:32 +0800

    Fix extension to work on Chrome and Firefox

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,32 +1,22 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-		if (document.readyState === "complete" && document.querySelector('#podcastDownload').getAttribute("style")) {
-			clearInterval(readyStateCheckInterval);
+var readyStateCheckInterval = setInterval(function() {
+  console.log('test');
+  if (document.readyState === "complete" && document.getElementById('podcastDownload').getAttribute("style")) {
+    clearInterval(readyStateCheckInterval);
 
-			function isDownloadable(element){
-				return !element.getAttribute("style").includes("hidden");
-			}
+    function isDownloadLinkHidden(element) {
+      return element.getAttribute("style").includes("hidden");
+    }
 
-			console.log("[+] hello there :-) ");
-			console.log("[+] thank you for using NUSCast Downloader!!!");
-			console.log("[+] you can log issues @ https://github.com/jiachen247/NUSCastDownloader");
+    console.log("[+] hello there :-) ");
+    console.log("[+] thank you for using NUSCast Downloader!!!");
+    console.log("[+] you can log issues @ https://github.com/jiachen247/NUSCastDownloader");
 
-			/*
-				JQuery Impl
-				$("#podcastDownload").attr({
-					style: "display: inline-block;",
-					target: "_blank",
-					href: $("meta[name='twitter:player:stream']").attr("content")
-				});
-			*/
+    const e = document.getElementById('podcastDownload');
 
-			const e = document.querySelector('#podcastDownload');
-
-			if(!isDownloadable(e)){
-				e.setAttribute("style", "display: inline-block;");
-				e.setAttribute('target', '_blank');
-				e.setAttribute('href', document.querySelector("meta[name='twitter:player:stream']").getAttribute("content"));
-			}
-		}
-	}, 10);
-});
+    if (isDownloadLinkHidden(e)) {
+      e.removeAttribute('style');
+      const mp4Link = document.querySelector("meta[name='twitter:player:stream']").getAttribute("content");
+      e.onclick = () => window.open(mp4Link, '_blank');
+    }
+  }
+}, 500);
